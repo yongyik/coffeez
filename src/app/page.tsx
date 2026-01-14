9ff03ecf1ce527f3a3bbf3230567ed9c4345d49b
YONG YIK <tangyongyik1014@gmail.com>
2026-01-14 20:02:50 +0800

    feat(home): update Hero component”

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ export default function HomePage() {
   return (
     <div>
       <Hero
+        src="/images/home/hero.jpg"
+        alt="桌子和咖啡"
         title="手工烘焙 · 一杯温暖你的咖啡"
         desc="「我们是一家位于市中心的小咖啡馆，精选咖啡豆，用心冲泡每一杯。」"
       />
--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,12 +1,22 @@
+import Image from "next/image";
+
 interface HeroProps {
+  src: string;
+  alt: string;
   title: string;
   desc: string;
 }
-export default function Hero({ title, desc }: HeroProps) {
+export default function Hero({src,alt, title, desc }: HeroProps) {
   return (
-    <section className="p-5">
-      <h1>{title}</h1>
-      <p>{desc}</p>
-    </section>
+
+    <header className="relative w-full h-70">
+      
+      <Image src={src} alt={alt} fill className="object-cover" priority />
+      <div className="absolute inset-0 bg-black/40" />
+      <div className="absolute top-14 left-7 text-amber-50 flex flex-col gap-1.5">
+        <h1 className="text-2xl">{title}</h1>
+        <p className="text-sm w-69 h-auto">{desc}</p>
+      </div>
+    </header>
   );
 }
